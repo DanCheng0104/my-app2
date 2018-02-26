fb6ddf23e8b3646c6831ecfd5ea245db563f7259
DAN CHENG <dcheng0104@gmail.com>
2018-02-25 21:59:38 -0800

    try to change layer based on geo

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 // import logo from '../logo.svg';
 import PanelContent from './PanelContent';
 import Map from './Map';
+
+
 class App extends Component {
   constructor(){
     super();
+    this.updateFilter = this.updateFilter.bind(this);
     this.state ={
       map:{
         center:[ -118.382877, 34.014700],
@@ -12,15 +15,26 @@ class App extends Component {
       },
       layer : {
         filter: ["all",['==','YEAR',2011],['==','ID','neighborhoods_291']]
-      }
+      },
+      
     };
+  }
+
+  updateFilter(geo){
+    // const fishes = {...this.state.fishes};
+		// fishes[key] = fish;
+    // this.setState({fishes});
+    //const oldFilter = {...this.state.filter};
+    console.log(geo);
+    const newFilter =  ["all",['==','YEAR',2011],['==','ID',geo]];
+    this.setState({layer:newFilter});
   }
   render() {
     const BUTTONS = ['Default', 'Primary', 'Success', 'Info', 'Warning', 'Danger'];
     return (
       <div>
         <Map map={this.state.map} layer={this.state.layer}/>
-        <PanelContent/>
+        <PanelContent updateFilter={this.updateFilter}/>
       </div>
       
     );
--- src/components/PanelContent.js
+++ src/components/PanelContent.js
@@ -2,36 +2,35 @@ import React, { Component } from 'react';
 // import logo from '../logo.svg';
 import '../css/PanelContent.css';
 import { DropdownButton, MenuItem, Panel } from 'react-bootstrap';
-
-// <DropdownButton
-// bsStyle='default'
-// title='Default'
-// id ='1'
-// className = 'nbOption'
-// >
-// <MenuItem eventKey="1">Action</MenuItem>
-// <MenuItem eventKey="2">Another action</MenuItem>
-// <MenuItem eventKey="3" active>
-//   Active Item
-// </MenuItem>
-// <MenuItem divider />
-// <MenuItem eventKey="4">Separated link</MenuItem>
-// </DropdownButton>
+import data from '../geo.json';
 
 class App extends Component {
   constructor(){
     super();
+    this.handlerChange = this.handlerChange.bind(this);
+  }
+  handlerChange(e,key){
+      this.props.updateFilter(key);
+
+
   }
   render() {
     const BUTTONS = ['Default', 'Primary', 'Success', 'Info', 'Warning', 'Danger'];
+    const geos = data;
+    const geoKeys = Object.keys(geos);
     return (
         <div className = "paneltest">
             <Panel>
-                <Panel.Heading>Panel heading without a title</Panel.Heading>
+                <Panel.Heading>The City Of LA Water Consumption Map</Panel.Heading>
                 <Panel.Body>
-                    <div>Panel content</div>
-                    <div>render</div>
-                    <div>render</div>
+                <DropdownButton
+                bsStyle='default'
+                title='Please choose a neighborhood below'
+                id ='1'
+                className = 'nbOption'
+                >
+                    {geoKeys.map(geoKey=><MenuItem key={geoKey} eventKey={geoKey}  onClick={(e)=>this.handlerChange(e,geoKey)}>{geos[geoKey]}</MenuItem>)}	
+                </DropdownButton>
                 </Panel.Body>
             </Panel>
         </div>
@@ -39,5 +38,9 @@ class App extends Component {
     );
   }
 }
+
+// <MenuItem eventKey="1">Action</MenuItem>
+// <MenuItem eventKey="2">Another action</MenuItem>
+// <MenuItem eventKey="3" active>Active Item</MenuItem>
 
 export default App;
